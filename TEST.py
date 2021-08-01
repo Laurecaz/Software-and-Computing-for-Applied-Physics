dbdecffeb74ef9d278962f708b88b1cf1dddf5ad
Laurecaz <61115979+Laurecaz@users.noreply.github.com>
2021-08-01 12:37:01 +0200

    remove test function max3array

--- TEST.py
+++ TEST.py
@@ -1,11 +1,5 @@
 import pytest
-from functions import maximum_3array
+from functions import hydrogen_wf, separation_by_probability
 import numpy as np
 
 
-#Test of maximum_3array
-def test_max3array() :
-    #Does it work at it should be :
-    assert maximum_3array([6,0,1],[1,2,4],[1,6,3]) == 6
-    #Does it work with different array lenghts :
-    assert maximum_3array([1,6],[1],[1,6,3]) == 6
