709708b4230c8dd3a800b5e85074b9b5fc3ef256
Laurecaz <61115979+Laurecaz@users.noreply.github.com>
2021-08-02 13:58:34 +0200

    Plotting without the import of our data

--- plots.py
+++ plots.py
@@ -1,1 +1,14 @@
+#plotting
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+#Positive : 
+ax.scatter(low_prob_positive[0],low_prob_positive[1],low_prob_positive[2], alpha=0.05, s=2,color='lightsteelblue')
+ax.scatter(middle_prob_positive[0],middle_prob_positive[1],middle_prob_positive[2], alpha=0.05, s=2,color='blue')
+ax.scatter(high_prob_positive[0],high_prob_positive[1],high_prob_positive[2], alpha=0.05, s=2,color='midnightblue')
+
+#Negative : 
+ax.scatter(low_prob_negative[0],low_prob_negative[1],low_prob_negative[2], alpha=0.05, s=2,color='lightsteelblue')
+ax.scatter(middle_prob_negative[0],middle_prob_negative[1],middle_prob_negative[2], alpha=0.05, s=2,color='blue')
+ax.scatter(high_prob_negative[0],high_prob_negative[1],high_prob_negative[2], alpha=0.05, s=2,color='midnightblue')
 
