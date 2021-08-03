d8bf828fef63b859c096986ee7f3d501ba3c4cf1
Laurecaz <61115979+Laurecaz@users.noreply.github.com>
2021-08-03 18:18:33 +0200

    test of sign, wf_to_proba and hydrogen_wf

--- TEST.py
+++ TEST.py
@@ -1,5 +1,28 @@
-import pytest
-from functions import hydrogen_wf, separation_by_probability
+from functions import From_wf_to_probability, sign_separation, separation_by_probability, hydrogen_wf
 import numpy as np
+import random
+import math
+from simulation import positive_prob
+
+
+def test_hydrogen_wf():
+    # check that some specific case that we know
+    assert hydrogen_wf(2,1,0,0,0,0) == (1e-12+1e-12j) 
+    assert hydrogen_wf(2,1,1,0,0,0) == (1e-12+1e-12j)
+    assert hydrogen_wf(3,1,0,0,0,0) == (1e-12+1e-12j)
+    assert hydrogen_wf(3,2,1,0,0,0) == (1e-12+1e-12j)
+    
+
+ def test_wf_to_probability():
+    # sum of all the data equals to one to check that it returns a probability
+    assert round(sum(From_wf_to_probability(np.random.random(1000))),10) == 1
+    # all the data are positive to check that it returns  a probability
+    for i in range(1000):
+        assert From_wf_to_probability(np.random.random(1000))[i] >= 0
+        
+        
+def test_sign():
+    # test for the 0
+    assert sign_separation([-1,-2,45,5,0,-9.78,0.01]) == [[45,5,0,0.01],[-1,-2,-9.78]]
 
 
