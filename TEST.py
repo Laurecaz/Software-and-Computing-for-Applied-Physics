d3fb0eafbc65796ea76fd1d5d460c9ed840bdce5
Laurecaz <61115979+Laurecaz@users.noreply.github.com>
2021-08-04 22:50:39 +0200

    Update TEST.py

--- TEST.py
+++ TEST.py
@@ -1,8 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 12 15:24:26 2021
+
+@author: laurecazals
+"""
+
 from functions import From_wf_to_probability, sign_separation, separation_by_probability, hydrogen_wf
 import numpy as np
 import random
 import math
-from simulation import positive_prob
+from simulation_config import positive_prob
 
 
 def test_hydrogen_wf():
@@ -11,18 +19,18 @@ def test_hydrogen_wf():
     assert hydrogen_wf(2,1,1,0,0,0) == (1e-12+1e-12j)
     assert hydrogen_wf(3,1,0,0,0,0) == (1e-12+1e-12j)
     assert hydrogen_wf(3,2,1,0,0,0) == (1e-12+1e-12j)
-    
-    
+
+
 def test_separation_by_probability():
-    # an example to test a property 
+    # test that the output have the same lenght
     coord = np.linspace(-10,10,len(positive_prob))
     maximum = max(positive_prob)
     x, y, z = separation_by_probability(0.25*maximum,0.50*maximum,coord,coord,coord,positive_prob)
     assert len(x) == len(y)
     assert len(y) == len(z)
-   
-
- def test_wf_to_probability():
+    
+    
+def test_wf_to_probability():
     # sum of all the data equals to one to check that it returns a probability
     assert round(sum(From_wf_to_probability(np.random.random(1000))),10) == 1
     # all the data are positive to check that it returns  a probability
@@ -33,5 +41,9 @@ def test_separation_by_probability():
 def test_sign():
     # test for the 0
     assert sign_separation([-1,-2,45,5,0,-9.78,0.01]) == [[45,5,0,0.01],[-1,-2,-9.78]]
+    
+
+if __name__ == "main":
+    pass 
 
 
