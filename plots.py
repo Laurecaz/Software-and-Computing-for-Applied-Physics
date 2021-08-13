0e121cec645b449c4e47932e871edd9024216383
Laurecaz <61115979+Laurecaz@users.noreply.github.com>
2021-08-13 15:19:14 +0200

    Config instead of import data from simulation.py
    Separation of the configuration file for the simulation and the plot files.

--- plots.py
+++ plots.py
@@ -6,35 +6,45 @@ Created on Mon Aug  2 14:26:03 2021
 
 import numpy as np
 import matplotlib.pyplot as plt
-from functions import update, modelisation
+from functions import modelisation
 from matplotlib.collections import PathCollection
 from matplotlib.legend_handler import HandlerPathCollection, HandlerLine2D
 import configparser
-from simulation import positive_prob, negative_prob
-
+import random
 
 
 config = configparser.ConfigParser()
-config.read('configuration.txt')
+
+#Choice of the specific configuration
+chosen_text = input('Choose your plot configuration text : ')
+chosen_text = str(chosen_text)
+config.read(chosen_text)
 
 n = int(config.get('settings', 'n'))
 l = int(config.get('settings', 'l'))
 m = int(config.get('settings', 'm'))
 
-numberCoord = int(config.get('settings', 'numberCoord')) # need to be increase fo n = 1
+numberCoord = int(config.get('settings', 'numberCoord')) # need to be increase for n = 1
 
 zmin = int(config.get('settings', 'zmin'))
 zmax = int(config.get('settings', 'zmax'))
 
-low = float(config.get('settings', 'low_prob'))*100
-middle = float(config.get('settings', 'middle_prob'))*100
-high = float(config.get('settings', 'high_prob'))*100
+low = float(config.get('settings', 'low_prob'))
+middle = float(config.get('settings', 'middle_prob'))
+high = float(config.get('settings', 'high_prob'))
 
+random.seed(config.get('settings','NumbSeed'))
+
+#import the data created in the simulation file
+source1 = config.get('paths','positive_prob')
+source2 = config.get('paths','negative_prob')
 
 destination = config.get('paths',f'orbitals_{n}{l}{m}_pic')
 
 
 
+positive_prob = np.load(source1)
+negative_prob = np.load(source2)
 
 #Arbitrary coordinates with the same size as our positive and negative probability, usefull to create our random coordinates following our probability
 coord_pos = np.linspace(zmin,zmax,len(positive_prob))
@@ -44,16 +54,22 @@ coord_neg = np.linspace(zmin,zmax,len(negative_prob))
 # Creation of our coordinates generated randomly following the probability (either "positive" or "negative one) trend + Separation of the coordinates for different probability range.
 #"Positive" coordinates :
 
-low_prob_positive = modelisation(positive_prob,coord_pos,n,l,m,numberCoord,1)[0]
-middle_prob_positive = modelisation(positive_prob,coord_pos,n,l,m,numberCoord,1)[1]
-high_prob_positive = modelisation(positive_prob,coord_pos,n,l,m,numberCoord,1)[2]
+low_prob_positive = modelisation(positive_prob,coord_pos,n,l,m,numberCoord,1,low,middle,high)[0]
+middle_prob_positive = modelisation(positive_prob,coord_pos,n,l,m,numberCoord,1,low,middle,high)[1]
+high_prob_positive = modelisation(positive_prob,coord_pos,n,l,m,numberCoord,1,low,middle,high)[2]
 
 #"Negative" coordinates :
-low_prob_negative = modelisation(negative_prob,coord_neg,n,l,m,numberCoord,-1)[0]
-middle_prob_negative = modelisation(negative_prob,coord_neg,n,l,m,numberCoord,-1)[1]
-high_prob_negative = modelisation(negative_prob,coord_neg,n,l,m,numberCoord,-1)[2]
+low_prob_negative = modelisation(negative_prob,coord_neg,n,l,m,numberCoord,-1,low,middle,high)[0]
+middle_prob_negative = modelisation(negative_prob,coord_neg,n,l,m,numberCoord,-1,low,middle,high)[1]
+high_prob_negative = modelisation(negative_prob,coord_neg,n,l,m,numberCoord,-1,low,middle,high)[2]
 
 
+def update(handle, orig):
+    """ This method is used to set the marker transparency. 
+    Only solution find on internet to handle this issue.
+    """
+    handle.update_from(orig)
+    handle.set_alpha(1)
 
 
 #Plotting
@@ -66,9 +82,9 @@ def graphPlot():
 
     #Positive wavefunction data: 
         
-    ax.scatter(low_prob_positive[0],low_prob_positive[1],low_prob_positive[2], alpha=0.05, s=2,color='cornflowerblue',label=f'Between {low}% and {middle}%')
-    ax.scatter(middle_prob_positive[0],middle_prob_positive[1],middle_prob_positive[2], alpha=0.05, s=2,color='blue',label=f'Between {middle}% and {high}%')
-    ax.scatter(high_prob_positive[0],high_prob_positive[1],high_prob_positive[2], alpha=0.05, s=2,color='red', label=f'More than {high}%')
+    ax.scatter(low_prob_positive[0],low_prob_positive[1],low_prob_positive[2], alpha=0.05, s=2,color='cornflowerblue',label=f'Between {low*100}% and {middle*100}%')
+    ax.scatter(middle_prob_positive[0],middle_prob_positive[1],middle_prob_positive[2], alpha=0.05, s=2,color='blue',label=f'Between {middle*100}% and {high*100}%')
+    ax.scatter(high_prob_positive[0],high_prob_positive[1],high_prob_positive[2], alpha=0.05, s=2,color='red', label=f'More than {high*100}%')
 
     #Negative wavefunction data:
     ax.scatter(low_prob_negative[0],low_prob_negative[1],low_prob_negative[2], alpha=0.05, s=2,color='cornflowerblue')
